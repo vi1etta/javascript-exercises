10e73e04ce18407e9f042f2f0fc29b816d23fbae
vi1etta <violet.samko@gmail.com>
2025-03-26 21:08:37 +0200

    Add functions to pass the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  return Math.round((10 * (fahr - 32) * 5) / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cels) {
+return Math.round(((cels*9/5)+32)*10)/10;
+
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
