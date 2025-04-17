8c9200ecb44ec2fadf49b8343036cf429d42e2f1
vi1etta <violet.samko@gmail.com>
2025-04-17 11:29:07 +0300

    Add solution to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,35 +1,28 @@
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-];
-
-const findOldest = findTheOldest(people)
-
 function findTheOldest(array) {
-  return  array.reduce((oldest, current) => {
-     const ageOldest = oldest.yearOfDeath - oldest.yearOfBirth;
-     const ageCurrent = current.yearOfDeath -current.yearOfBirth;
-     if ( ageCurrent > ageOldest) {
-       return current;
-     } else{
-       return oldest;
-     }
-   });
-  
- }
+  return array.reduce((oldest, current) => {
+    let currentDate = new Date();
+    let ageCurrent;
+    let ageOldest;
+    if (!oldest.yearOfDeath) {
+      ageOldest = currentDate.getFullYear() - oldest.yearOfBirth;
+    } else {
+      ageOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+    }
+
+    if (!current.yearOfDeath) {
+      ageCurrent = currentDate.getFullYear() - current.yearOfBirth;
+    } else {
+      ageCurrent = current.yearOfDeath - current.yearOfBirth;
+    }
+
+    if (ageCurrent > ageOldest) {
+      return current;
+    }  
+    else {
+      return oldest;
+    }
+  });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
